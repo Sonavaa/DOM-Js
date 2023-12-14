28b19526b802fc2668d34f185d312fb9f3161afd
Sonava <sonava@code.edu.az>
2023-12-14 16:13:37 +0400

    added RCG

--- RCG/script.js
+++ RCG/script.js
@@ -1,1 +1,11 @@
-"use strict";
+"use strict"
+const button = document.getElementById("btn")
+const body_el = document.querySelector("body")
+const generateRandomNumber = () =>{
+return Math.floor(Math.random() * 256)
+}
+
+button.addEventListener("click", () => {
+    const randomNumber = generateRandomNumber()
+    body_el.style.backgroundColor = `rgb(${randomNumber}, ${randomNumber}, ${randomNumber})`
+})
